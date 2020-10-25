918b7ff082231f01f658b736bc4ec2db0f4a5d75
nkrasner <61438839+nkrasner@users.noreply.github.com>
2020-10-25 11:16:35 -0400

    Delete background.js

--- background.js
+++
@@ -1,5 +0,0 @@
-//Background scripts
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.local.clear();
-    chrome.storage.local.set({"groups":{}}); //Start with empty group list
-});
