e9cce95e9d90795fe544a768b74c864c4c3c4636
nkrasner <61438839+nkrasner@users.noreply.github.com>
2020-10-27 15:47:54 -0400

    Add files via upload
    Beginnings of automating group creation

--- background.js
+++ background.js
@@ -2,4 +2,5 @@
 chrome.runtime.onInstalled.addListener(function() {
     chrome.storage.local.clear();
     chrome.storage.local.set({"groups":{}}); //Start with empty group list
+    const urlGraph = require("./urlGraph");
 });
