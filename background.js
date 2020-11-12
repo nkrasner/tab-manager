6755bbc145554146786af02876e5c2295e6b1e4f
nkrasner <61438839+nkrasner@users.noreply.github.com>
2020-11-12 14:10:15 -0500

    Add files via upload

--- background.js
+++ background.js
@@ -1,6 +1,21 @@
+const int MAX_NODES = 1000;
 //Background scripts
 chrome.runtime.onInstalled.addListener(function() {
     chrome.storage.local.clear();
-    chrome.storage.local.set({"groups":{}}); //Start with empty group list
-    //const urlGraph = require("./urlGraph");
+    //Start with empty group list and graph
+    chrome.storage.local.set({
+        "groups":{},
+        "urlGraph":new urlGraph(),
+        "number":0
+    });
 });
+
+/*chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+
+    chrome.storage.local.get("urlGraph", function(urlGraph) {
+        if(!urlGraph.isNode(tab.url)) {
+            addNode(tab.url);
+
+        }
+    });
+});*/
