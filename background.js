8f7f74d9e99cdb583213270a4051196ce38fd4b8
nkrasner <61438839+nkrasner@users.noreply.github.com>
2020-11-15 02:29:54 -0500

    Add files via upload
    removed graph

--- background.js
+++ background.js
@@ -1,25 +1,11 @@
-const MAX_NODES = 1000;
+
 //Background scripts
 chrome.runtime.onInstalled.addListener(function() {
     chrome.storage.local.clear();
-    //Start with empty group list and graph
+    //Start with empty group list
     chrome.storage.local.set({
         "groups":{},
-        "urlGraph":new urlGraph(),
         "number":0,
         "edit":false
     });
 });
-
-/*
-
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-
-    chrome.storage.local.get("urlGraph", function(urlGraph) {
-        if(!urlGraph.isNode(tab.url)) {
-            addNode(tab.url);
-
-        }
-    });
-});
-*/
